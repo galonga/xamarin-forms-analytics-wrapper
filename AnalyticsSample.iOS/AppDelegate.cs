751b4161840a196b706a91b0151964be8e017667
Your Name <your.name@reBuy.de>
2016-11-15 20:10:46 +0100

    Refactor code
    Init commit
    
    Add iOS analytics service
    
    Refactor project structure and add sample app
    
    Add Readme and nuspec file
    
    Init commit

---
+++ AnalyticsSample.iOS/AppDelegate.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using UIKit;
+using XamarinFormsAnalyticsWrapper.iOS.Services;
+
+namespace AnalyticsSample.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            var gaService = AnalyticsService.GetGASInstance();
+            gaService.Init("UA-87598000-1", 5);
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ XamarinFormsAnalyticsWrapper.iOS/Mapper/AnalyticsProductMapper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using XamarinFormsAnalyticsWrapper.Models;
+using Foundation;
+using Google.Analytics;
+
+namespace XamarinFormsAnalyticsWrapper.iOS.Mapper
+{
+    public class AnalyticsProductMapper
+    {
+        public EcommerceProduct mapProduct (ProductData p)
+        {
+            var product = new EcommerceProduct();
+
+            if (p.Brand != null) {
+                product.SetBrand (p.Brand);
+            } else {
+                product.SetBrand ("none");
+            }
+            if (p.Category != null) {
+                product.SetCategory (p.Category);
+            }
+            if (p.Coupon != null) {
+                product.SetCouponCode (p.Coupon);
+            }
+            if (p.Id != null) {
+                product.SetId (p.Id);
+            }
+            if (p.Name != null) {
+                product.SetName (p.Name);
+            }
+            if (p.Position != 0) {
+                product.SetPosition (new NSNumber (p.Position));
+            }
+            if (p.Price != 0) {
+                product.SetPrice (new NSNumber (p.Price));
+            }
+            if (p.Quantity != 0) {
+                product.SetQuantity (new NSNumber (p.Quantity));
+            }
+            if (p.Variant != null) {
+                product.SetVariant (p.Variant);
+            }
+
+            return product;
+        }
+    }
+}
